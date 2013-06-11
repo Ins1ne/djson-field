2f0de1cd0a2988fdb58d124fe16c28c7bcd18990
Vladislav Lozhechnik <=>
2013-06-11 18:13:37 +0300

    refactoring

--- djson_field/widgets.py
+++ djson_field/widgets.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.forms import fields
 from django.forms.widgets import Textarea
 from django.template.loader import render_to_string
 
@@ -40,6 +41,15 @@ def remove_empty_items(tree):
 
 class JSONWidget(Textarea):
     def __init__(self, rules=None, **kwargs):
+        rules = rules or []
+        self.rules = [
+            ([r".*"], {
+                'type': fields.CharField()
+            }),
+            ([r"key3", r".*"], {
+                'type': fields.IntegerField()
+            })
+        ] + rules
         super(JSONWidget, self).__init__(**kwargs)
 
     def add_links_template(self):
@@ -57,33 +67,39 @@ class JSONWidget(Textarea):
         add_link += u'</li>'
         return add_link
 
-    def render_data(self, name, data):
-        html = u""
+    def render_dict(self, name, data):
+        items = {}
+        for key, value in data.iteritems():
+            items[key] = self.render_data("%s[%s]" % (name, "_" + key), value)
+        return render_to_string("djson_field/dictionary_item.html", {
+            'name': name,
+            'items': items,
+            'controls': self.add_links_template()
+        })
 
+    def render_list(self, name, data):
+        items = []
+        for i in xrange(len(data)):
+            items.append(self.render_data("%s[%i]" % (name, i), data[i]))
+        return render_to_string("djson_field/list_item.html", {
+            'name': name,
+            'items': items,
+            'controls': self.add_links_template()
+        })
+
+    def render_plain(self, name, data):
+        return render_to_string("djson_field/plain_item.html", {
+            'name': name,
+            'value': unicode(data)
+        })
+
+    def render_data(self, name, data):
         if type(data) == dict:
-            items = {}
-            for key, value in data.iteritems():
-                items[key] = self.render_data("%s[%s]" % (name, "_" + key), value)
-            html = render_to_string("djson_field/dictionary_item.html", {
-                'name': name,
-                'items': items,
-                'controls': self.add_links_template()
-            })
+            return self.render_dict(name, data)
         elif type(data) == list:
-            items = []
-            for i in xrange(len(data)):
-                items.append(self.render_data("%s[%i]" % (name, i), data[i]))
-            html = render_to_string("djson_field/list_item.html", {
-                'name': name,
-                'items': items,
-                'controls': self.add_links_template()
-            })
+            return self.render_list(name, data)
         else:
-            html = render_to_string("djson_field/plain_item.html", {
-                'name': name,
-                'value': unicode(data)
-            })
-        return html
+            return self.render_plain(name, data)
 
     def render(self, name, value, attrs=None):
         json_dict = {}
@@ -91,20 +107,9 @@ class JSONWidget(Textarea):
             json_dict = json.loads(value)
         html = self.render_data(name, json_dict)
         templates = {
-            'dict': render_to_string("djson_field/dictionary_item.html", {
-                'name': '%%NAME%%',
-                'items': {},
-                'controls': self.add_links_template()
-            }),
-            'list': render_to_string("djson_field/list_item.html", {
-                'name': '%%NAME%%',
-                'items': [],
-                'controls': self.add_links_template()
-            }),
-            'plain': render_to_string("djson_field/plain_item.html", {
-                'name': '%%NAME%%',
-                'value': ""
-            })
+            'dict': self.render_dict('%%NAME%%', {}),
+            'list': self.render_list('%%NAME%%', []),
+            'plain': self.render_plain('%%NAME%%', u"")
         }
         return render_to_string("djson_field/base.html", {
             'content': html,
