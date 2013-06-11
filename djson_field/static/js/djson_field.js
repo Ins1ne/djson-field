ee67537ca0ae3baff75b9f9c6e07c874ddf914a8
Vladislav Lozhechnik <=>
2013-06-11 15:31:00 +0300

    adding, editing, removing of items: plain items, list items and dictionary items

--- djson_field/static/js/djson_field.js
+++ djson_field/static/js/djson_field.js
@@ -1,3 +1,47 @@
 $(document).ready(function(e){
-	alert('aaa');
+	$('.jsonFieldWidget').delegate('.jsonFieldAddLink a', 'click', function(e){
+		e.preventDefault();
+		var list = $(this).parent().parent();
+		var last = list.children('.jsonFieldItem').last();
+		isLabel = true;
+		if(list.hasClass('jsonFieldDict')){
+			var key = prompt("Введите ключ");
+			var name = list.attr('name') + '[_' + key + ']';
+		} else {
+			var key = 0;
+			if(last && last.length>0){
+				var indexes = getKeys(last.children('.jsonFieldItemValue').attr('name'));
+				if(indexes && indexes.length > 0)
+					key = parseInt(indexes.pop()) + 1;
+			}
+			isLabel = false;
+			var name = list.attr('name') + '[' + key + ']';
+		}
+		var label = "<label>" + key + "</label>";
+		if($(this).hasClass("plain")){
+			var item = $('.jsonFieldWidget').has($(this)).find(".templates>.plain").html().replace(/%%NAME%%/g, name);
+		} else if($(this).hasClass("list")){
+			var item = $('.jsonFieldWidget').has($(this)).find(".templates>.list").html().replace(/%%NAME%%/g, name);
+		} else if($(this).hasClass("dict")){
+			var item = $('.jsonFieldWidget').has($(this)).find(".templates>.dict").html().replace(/%%NAME%%/g, name);
+		}
+
+		var li = "<li class=\"jsonFieldItem\">" +
+					'<a href="#" class="deleteItem">Удалить</a> ' +
+					(isLabel ? label : "") + item + 
+					'</li>';
+		$(this).parent().before(li);
+	});
+	$('.jsonFieldWidget').delegate('.jsonFieldItem>.deleteItem', 'click', function(e){
+		e.preventDefault()
+		$(this).parent().remove();
+	});
 });
+
+function getKeys(str){
+	keys = str.match(/\[[a-zA-Z0-9_.]+\]/g);
+	if(keys && keys.length > 0){
+		for(var i=0; i<keys.length; i++) keys[i] = keys[i].slice(1,-1);
+	}
+	return keys
+}
--- djson_field/widgets.py
+++ djson_field/widgets.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.forms.widgets import Textarea
-from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
 
 import json
 import re
@@ -18,6 +18,8 @@ def update_tree(tree, path, value):
             if not tree:
                 tree = {}
             index = path[0]
+            if index.startswith("_"):
+                index = index[1:]
             if index not in tree:
                 tree[index] = None
         if len(path) == 1:
@@ -27,35 +29,87 @@ def update_tree(tree, path, value):
     return tree
 
 
+def remove_empty_items(tree):
+    if type(tree) == list:
+        return [remove_empty_items(ob) for ob in tree if ob is not None]
+    elif type(tree) == dict:
+        for key, value in tree.iteritems():
+            tree[key] = remove_empty_items(value)
+    return tree
+
+
 class JSONWidget(Textarea):
     def __init__(self, rules=None, **kwargs):
         super(JSONWidget, self).__init__(**kwargs)
 
+    def add_links_template(self):
+        separator = " | "
+        added_links = {
+            "plain": u'<a class="plain" href="#">простое значение</a>',
+            "list": u'<a class="list" href="#">список</a>',
+            "dict": u'<a class="dict" href="#">словарь</a>',
+        }
+        add_link = u'<li class="jsonFieldAddLink">Добавить: '
+        for i, link in enumerate(added_links.values()):
+            add_link += link
+            if i < len(added_links) - 1:
+                add_link += separator
+        add_link += u'</li>'
+        return add_link
+
     def render_data(self, name, data):
         html = u""
+
         if type(data) == dict:
+            items = {}
             for key, value in data.iteritems():
-                field_name = "%s[%s]" % (name, key)
-                label = u'<label for="id_%s">%s</label>' % (field_name, key)
-                field = self.render_data(field_name, value)
-                html += u"<li>" + label + field + u"</li>"
-            html = u"<ul>" + html + u"</ul>"
+                items[key] = self.render_data("%s[%s]" % (name, "_" + key), value)
+            html = render_to_string("djson_field/dictionary_item.html", {
+                'name': name,
+                'items': items,
+                'controls': self.add_links_template()
+            })
         elif type(data) == list:
+            items = []
             for i in xrange(len(data)):
-                html += u"<li>" + self.render_data("%s[%i]" % (name, i), data[i])
-            html = u"<ol>" + html + u"</ol>"
+                items.append(self.render_data("%s[%i]" % (name, i), data[i]))
+            html = render_to_string("djson_field/list_item.html", {
+                'name': name,
+                'items': items,
+                'controls': self.add_links_template()
+            })
         else:
-            html = u'<input id="id_%(name)s" name="%(name)s" value="%(value)s" />' % {
+            html = render_to_string("djson_field/plain_item.html", {
                 'name': name,
                 'value': unicode(data)
-            }
-        return mark_safe(html)
+            })
+        return html
 
     def render(self, name, value, attrs=None):
         json_dict = {}
         if value and len(value) > 0:
             json_dict = json.loads(value)
-        return self.render_data(name, json_dict)
+        html = self.render_data(name, json_dict)
+        templates = {
+            'dict': render_to_string("djson_field/dictionary_item.html", {
+                'name': '%%NAME%%',
+                'items': {},
+                'controls': self.add_links_template()
+            }),
+            'list': render_to_string("djson_field/list_item.html", {
+                'name': '%%NAME%%',
+                'items': [],
+                'controls': self.add_links_template()
+            }),
+            'plain': render_to_string("djson_field/plain_item.html", {
+                'name': '%%NAME%%',
+                'value': ""
+            })
+        }
+        return render_to_string("djson_field/base.html", {
+            'content': html,
+            'templates': templates
+        })
 
     def value_from_datadict(self, data, files, name):
         value = {}
@@ -63,6 +117,7 @@ class JSONWidget(Textarea):
             if key.startswith(name + "["):
                 branch_keys = re.findall(r'\[([A-Za-z0-9_.]*)\]', key)
                 value = update_tree(value, branch_keys, val)
+        value = remove_empty_items(value)
         return json.dumps(value)
 
     class Media:
