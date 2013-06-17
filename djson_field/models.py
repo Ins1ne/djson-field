ebedeacc2dc960f84545177f93b9be4afbdd9d4b
Vladislav Lozhechnik <=>
2013-06-17 18:14:28 +0300

    some changes

--- djson_field/models.py
+++ djson_field/models.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.forms import CharField
+from django.forms import CharField, IntegerField
 from djson_field.forms import JsonField
 
+import json
 import re
 _ = re.compile
 
@@ -13,8 +14,8 @@ class JSONField(models.TextField):
 
     description = 'JSON object'
     BASE_RULES = [
-        ([], {
-            'type': CharField(),
+        (['key'], {
+            'type': IntegerField(),
             'actions': ['add_plain'],
             'allow_item_removing': False
         }),
@@ -23,7 +24,11 @@ class JSONField(models.TextField):
     def __init__(self, *args, **kwargs):
         self.rules = self.BASE_RULES
         if 'rules' in kwargs:
-            self.rules += kwargs['rules']
+            add_rules = kwargs['rules']
+            if hasattr(add_rules, '__call__'):
+                self.rules += add_rules()
+            else:
+                self.rules += add_rules
             del kwargs['rules']
         super(JSONField, self).__init__(*args, **kwargs)
 
@@ -33,5 +38,20 @@ class JSONField(models.TextField):
         defaults.update(kwargs)
         return super(JSONField, self).formfield(**defaults)
 
-    def validate(self, value, *args, **kwargs):
-        super(JSONField, self).validate(*args, **kwargs)
+    def validate_item(self, item, model_instance, path):
+        errors = []
+        if isinstance(item, dict):
+            for key, subitem in item.iteritems():
+                errors += self.validate_item(subitem, model_instance, path + [key])
+        elif isinstance(item, list):
+            for subitem in item:
+                errors += self.validate_item(subitem, model_instance, path)
+        else:
+            field = self.formfield()
+        return errors
+        
+
+    def validate(self, value, model_instance):
+        item = json.loads(value)
+        errors = self.validate_item(item, model_instance)
+        super(JSONField, self).validate(value, model_instance)
--- example/app/models.py
+++ example/app/models.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.forms import CharField
 from djson_field.models import JSONField
+
+
+def get_rules():
+    return [
+        ([], {
+            'type': CharField(),
+            'actions': ['add_plain'],
+            'allow_item_removing': False
+        }),
+    ]
 
 
 class JSONModel(models.Model):
     title = models.CharField('Назва', max_length=255)
-    json_field = JSONField('JSON field')
+    json_field = JSONField('JSON field', rules=get_rules)
